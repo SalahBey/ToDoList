5624b410fcedace5dfdb3bd7bd586409b43f3c3a
FlorianB8 <billaultflo@gmail.com>
2022-12-08 16:52:56 +0100

    JE SUIS LE BOSSS

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,10 +7,7 @@ let subLists = [];
 if(localStorage.getItem('lists')){
     lists = JSON.parse(localStorage.getItem('lists'))
 } 
-if(localStorage.getItem('lists')){
-    subLists = JSON.parse(localStorage.getItem('lists'))
-}
-
+console.log(lists[1]);
 function displayList() {
     lists.forEach((listName, index) => {
         let nbList = index+1;
@@ -21,7 +18,7 @@ function displayList() {
         ` 
         <div id="list${nbList}" class="my-4 card">
             <div class="d-flex justify-content-between card-header align-items-center">
-                <input type="text" id="nameList${nbList}" class="listTitle border-0" name="listTitle" readonly value="${listName.name}">
+                <input type="text" id="nameList${nbList}" class="listTitle border-0" name="listTitle" disabled readonly value="${listName.name}">
                 <div class="d-flex justify-content-around">
                     <input id="updateList${nbList}" name="updateList${nbList}" class="mx-lg-3 btnChangeTitle rounded"
                         type="image" src="./public/assets/img/pencil.png">
@@ -34,22 +31,13 @@ function displayList() {
             <div class="d-flex flex-column card-body rounded-bottom">
                 <div class="elementList">
                     <div class="d-flex justify-content-between elementItem align-items-center">
-                        <input type="text" id="listElement" class="subListTitle card-text m-0 listElement" name="listElement" required placeholder="Ajouter élément">
+                        <input type="text" id="listElement${nbList}" class="subListTitle card-text m-0 listElement" name="listElement${nbList}" disabled readonly value="${listName.subList}">
                         <div class="d-flex justify-content-around">
-                            <input name="changeElementText" id="changeElementText"
+                            <input name="changeElementText${nbList}" id="changeElementText${nbList}"
                                 class="btnChangeElementText mx-lg-2 rounded" type="image"
                                 src="./public/assets/img/pencilelement.png">
-                            <input name="deleteElementText" id="deleteElementText"
-                                class="btnDeleteElementText mx-lg-2 rounded" type="image"
-                                src="./public/assets/img/trashelement.png">
                         </div>
-                        
                     </div>
-                </div>
-                <div class="d-flex align-items-center mt-2">
-                    <input name="addElement${nbList}" id="addElement${nbList}" class="btnAddElement rounded" type="button"
-                    value=" + ">
-                    <p class="addElementText mx-2 pt-3">Ajouter un élément</p>
                 </div>
             </div>
         </div>`;
@@ -59,6 +47,67 @@ function displayList() {
 }
 displayList();
 
+function updateList(){
+    lists.forEach((list,index)=> {
+        let btnListUpdate = document.querySelectorAll('.btnChangeTitle');
+
+        btnListUpdate.forEach((btnUpdate, index) => {
+            btnUpdate.addEventListener('click', () => {
+                let cpt = index+1;
+                let inputTitle = document.getElementById(`nameList${cpt}`);
+                inputTitle.removeAttribute('value');
+                inputTitle.setAttribute('placeholder','Entrez un nouveau titre')
+                let inputElement = document.getElementById(`listElement${cpt}`);
+                let valueElement = inputElement.value
+                inputTitle.removeAttribute('readonly');
+                inputTitle.removeAttribute('disabled');
+                
+                inputTitle.addEventListener('focusout', () => {
+                    let valueTitle = inputTitle.value;
+                    inputTitle.setAttribute('value', `${valueTitle}`);
+                    inputTitle.removeAttribute('placeholder');
+                    inputTitle.setAttribute('readonly', '');
+                    inputTitle.setAttribute('disabled', '');
+                        var lsValue = {
+                                id: cpt,
+                                name: valueTitle,
+                                subList : valueElement
+                            }
+                        lists[index] = lsValue;  
+                        localStorage.setItem('lists', JSON.stringify(lists)); 
+                    });
+                });
+            });    
+        })
+        let btnElementUpdate = document.querySelectorAll('.btnChangeElementText');
+        btnElementUpdate.forEach((btnUpdate, index) => {
+            btnUpdate.addEventListener('click', () => {
+                let cpt = index+1;
+                let inputTitle = document.getElementById(`nameList${cpt}`);
+                let inputElement = document.getElementById(`listElement${cpt}`);
+                inputElement.removeAttribute('value');
+                inputElement.setAttribute('placeholder','Entrez une nouvelle description')
+                inputElement.removeAttribute('readonly');
+                inputElement.removeAttribute('disabled');
+                inputElement.addEventListener('focusout', () => {
+                    let valueTitle = inputTitle.value;
+                    let valueDesc = inputElement.value;
+                    inputElement.setAttribute('value', `${valueDesc}`)
+                    inputElement.removeAttribute('placeholder');
+                    inputElement.setAttribute('readonly', '');
+                    inputElement.setAttribute('disabled', '');
+                    var lsValue = {
+                            id: cpt,
+                            name: valueTitle,
+                            subList : valueDesc
+                        }
+                        lists[index] = lsValue;   
+                    localStorage.setItem('lists', JSON.stringify(lists));
+                });    
+            })
+        })
+}
+updateList();
 
 function deleteList() {
     let btnListDelete = document.querySelectorAll('.btnDel');
@@ -79,8 +128,6 @@ deleteList();
 function validList() {
     let list = document.querySelectorAll('.listTitle');
     let nbList = parseInt(list.length) + 1;
-    let subList = document.querySelectorAll('.subListTitle')
-    let nbSubList = parseInt(subList.length) + 1;
     let div = document.createElement('div');
     div.id = 'divList'+nbList;
     div.className = 'col-12 col-lg-6';
@@ -101,40 +148,42 @@ function validList() {
         <div class="d-flex flex-column card-body rounded-bottom">
             <div class="elementList">
                 <div class="d-flex justify-content-between elementItem align-items-center">
-                    <input type="text" id="listElement${nbSubList}" class="subListTitle card-text m-0 listElement" name="listElement${nbSubList}" required placeholder="Ajouter élément">
+                    <input type="text" id="listElement${nbList}" class="subListTitle card-text m-0 listElement" name="listElement${nbList}" placeholder="Ajouter une description">
                     <div>
-                        <input name="changeElementText${nbSubList}" id="changeElementText${nbSubList}"
+                        <input name="changeElementText${nbList}" id="changeElementText${nbList}"
                             class="mx-lg-2 btnChangeElementText rounded" type="image"
                             src="./public/assets/img/pencilelement.png">
-                        <input name="deleteElementText${nbSubList}" id="deleteElementText${nbSubList}"
-                            class="mx-lg-2 btnDeleteElementText rounded" type="image"
-                            src="./public/assets/img/trashelement.png">
                     </div>
                 </div>
-            </div>
-            <div class="d-flex align-items-center mt-2">
-                <input name="addElement${nbList}" id="addElement${nbList}" class="btnAddElement rounded" type="button"
-                value=" + ">
-                <p class="addElementText mx-2 pt-3">Ajouter un élément</p>
             </div>
         </div>
     </div>`;
     divContainer.appendChild(div);
-    let input = document.getElementById(`nameList${nbList}`);
-    input.addEventListener('focusout', function (e) {
-        let value = input.value;
-        input.setAttribute('value', `${value}`);
-        input.removeAttribute('placeholder');
-        input.setAttribute('readonly', '');
-        input.setAttribute('disabled', '');
-        var lsValue = {
-                id: nbList,
-                name: value,
-                subList : []
-            }
-        lists.push(lsValue);  
-        localStorage.setItem('lists', JSON.stringify(lists)); 
+    let inputTitle = document.getElementById(`nameList${nbList}`);
+    let inputDesc = document.getElementById(`listElement${nbList}`);
+    inputTitle.addEventListener('focusout', () => {
+        let valueTitle = inputTitle.value;
+        inputTitle.setAttribute('value', `${valueTitle}`);
+        inputTitle.removeAttribute('placeholder');
+        inputTitle.setAttribute('readonly', '');
+        inputTitle.setAttribute('disabled', '');
+        inputDesc.addEventListener('focusout', () => {
+            let valueDesc = inputDesc.value;
+            console.log(valueDesc);
+            inputDesc.setAttribute('value', `${valueDesc}`)
+            inputDesc.removeAttribute('placeholder');
+            inputDesc.setAttribute('readonly', '');
+            inputDesc.setAttribute('disabled', '');
+            var lsValue = {
+                    id: nbList,
+                    name: valueTitle,
+                    subList : valueDesc
+                }
+            lists.push(lsValue);  
+            localStorage.setItem('lists', JSON.stringify(lists)); 
+        });
     });
+    
  
     deleteList();
 }
